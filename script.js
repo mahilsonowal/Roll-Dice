c8e72609e0a9fb0c98b3dfcfd078121ac3d91423
Mahil Sonowal <155747136+mahilsonowal@users.noreply.github.com>
2024-01-25 21:25:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+function rollDice() {
+    let dice1Divs = document.querySelectorAll(".dice1 div div");
+    let dice2Divs = document.querySelectorAll(".dice2 div div");
+    let winnerElement = document.querySelector(".winner");
+
+
+    let countDice1 = rollSingleDice(dice1Divs);
+
+
+    let countDice2 = rollSingleDice(dice2Divs);
+
+
+    if (countDice1 > countDice2) {
+        winnerElement.textContent = "Player 1 is the winner!";
+    } else if (countDice1 < countDice2) {
+        winnerElement.textContent = "Player 2 is the winner!";
+    } else {
+        winnerElement.textContent = "It's a tie!";
+    }
+}
+
+function rollSingleDice(div) {
+    let totalDiv = div.length; 
+
+  
+    div.forEach(innerDiv => {
+        innerDiv.style.display = "none";
+    });
+
+    
+    let rolledDivs = [];
+
+    
+    let randomCount = Math.floor(Math.random() * totalDiv) + 1;
+
+    
+    for (let i = 0; i < randomCount; i++) {
+
+        let randomIndex;
+
+      
+        do {
+            randomIndex = Math.floor(Math.random() * totalDiv);
+        } while (rolledDivs.includes(randomIndex));
+
+   
+        let randomInnerDiv = div[randomIndex];
+
+   
+        rolledDivs.push(randomIndex);
+
+        randomInnerDiv.style.display = "";
+    }
+
+   
+    return randomCount;
+}
